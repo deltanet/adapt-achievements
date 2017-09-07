9398800bbd373284130c8afeba2d386d78a088ee
RobertPeek <robert@delta-net.co.uk>
2017-09-07 16:35:53 +0100

    Fixes #24

--- js/achievements-drawer-view.js
+++ js/achievements-drawer-view.js
@@ -35,9 +35,9 @@ define(function(require) {
 
         checkCompletion: function() {
             if(Adapt.achievements.isAvailable){
-                this.$('.text-description').html(Adapt.course.get('_achievements')._drawer.certificateEnabled);
+                this.$('.text-description').html(Adapt.course.get('_achievements')._drawer.certificateEnabled).a11y_text();
             } else {
-                this.$('.text-description').html(Adapt.course.get('_achievements')._drawer.certificateDisabled);
+                this.$('.text-description').html(Adapt.course.get('_achievements')._drawer.certificateDisabled).a11y_text();
                 this.$('.certificate-container').hide();
                 this.$('.print-button').hide();
                 // Hide review
@@ -46,7 +46,7 @@ define(function(require) {
         },
 
         updateBody: function() {
-          this.$('.achievements-drawer-body').html(Adapt.achievements.bodyText);
+          this.$('.achievements-drawer-body').html(Adapt.achievements.bodyText).a11y_text();
         },
 
         populateCertificate: function() {
